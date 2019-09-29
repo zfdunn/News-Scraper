3010f0f7d8cd29ebaf2318303c860f5dd2c1f26a
Zach Dunn <zfdunn@comcast.net>
2019-09-29 15:17:57 -0400

    created heroku connection, created all directories needed

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const db = require("./models");
+const PORT = process.env.PORT || 8080;
+const app = express();
